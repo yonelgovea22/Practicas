efacb15a6d00a0b2f72af4fd31e02e68269fa91d
yonelgovea22 <36669199+yonelgovea22@users.noreply.github.com>
2018-02-22 09:33:50 -0600

    Add files via upload

---
+++ SemaforoPotenciometro.ino
@@ -0,0 +1,55 @@
+
+const int Sv = 6, Sa=5, Sr=4;
+const int Pv=2, Pr=3;
+const int btn=9;
+
+int boton;
+int valor = analogRead(A0);
+
+
+void setup() {
+  
+  pinMode(Sv, OUTPUT);
+  pinMode(Sa, OUTPUT);
+  pinMode(Sr, OUTPUT);
+  pinMode(Pv, OUTPUT);
+  pinMode(Pr, OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+boton=digitalRead(btn);
+if(boton==LOW){
+  digitalWrite(Sv,HIGH);
+  digitalWrite(Pr,HIGH);
+  
+  }else{
+    for(int i=0; i<3;i++){
+      digitalWrite(Sv,LOW);
+      delay(valor);
+      digitalWrite(Sv,HIGH);
+      delay(valor);
+      }
+      digitalWrite(Sv,LOW);
+      digitalWrite(Sa,HIGH);
+      delay(valor);
+      digitalWrite(Sa,LOW);
+      digitalWrite(Sr, HIGH);
+      digitalWrite(Pv,HIGH);
+      digitalWrite(Pr,LOW);
+      delay(valor);
+      digitalWrite(Sr,LOW);
+      digitalWrite(Pv,LOW);
+      digitalWrite(Pr,HIGH);
+  }
+    }
+
+
+
+
+
+  
+
